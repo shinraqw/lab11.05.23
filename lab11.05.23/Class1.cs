a8dcfd5c7ca70593c434a1c843bdd371b3942796
shinraqw <oliynik05vitaliy@gmail.com>
2023-05-11 19:47:04 +0300

    Add project files.

---
+++ lab11.05.23/Class1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab11._05._23
+{
+    public class Alphabet : IEnumerable
+    {
+        private char[] letters;
+
+        public Alphabet()
+        {
+            letters = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < letters.Length; i++)
+            {
+                yield return letters[i];
+            }
+        }
+    }
+    public class House : IEnumerable
+    {
+        private Apartment[] apartments;
+
+        public House(Apartment[] apartments)
+        {
+            this.apartments = apartments;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < apartments.Length; i++)
+            {
+                yield return apartments[i];
+            }
+        }
+    }
+    public class Apartment : IEnumerable
+    {
+        private string[] residents;
+
+        public Apartment(string[] residents)
+        {
+            this.residents = residents;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < residents.Length; i++)
+            {
+                yield return residents[i];
+            }
+        }
+    }
+    public class Garage : IEnumerable
+    {
+        private Car[] cars;
+
+        public Garage(Car[] cars)
+        {
+            this.cars = cars;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < cars.Length; i++)
+            {
+                yield return cars[i];
+            }
+        }
+    }
+
+    public class Car
+    {
+        private string make;
+        private string model;
+
+        public Car(string make, string model)
+        {
+            this.make = make;
+            this.model = model;
+        }
+
+        public override string ToString()
+        {
+            return make + " " + model;
+        }
+    }
+}
---
+++ lab11.05.23/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+
+namespace lab11._05._23
+{
+    internal class Program
+    {
+        class Program1
+        {
+            static void Main(string[] args)
+            {
+                Alphabet alphabet = new Alphabet();//1
+
+                foreach (char letter in alphabet)
+                {
+                    Console.Write(letter + " ");
+                }
+
+                Console.WriteLine("----");
+                Apartment[] apartments = new Apartment[]//2
+                    {
+                        new Apartment(new string[] { "John", "Mary" }),
+                        new Apartment(new string[] { "Bob", "Alice", "Charlie" }),
+                        new Apartment(new string[] { "David", "Emily" })
+                    };
+
+                House house = new House(apartments);
+
+                foreach (Apartment apartment in house)
+                {
+                    Console.Write("Apartment residents: ");
+                    foreach (string resident in apartment)
+                    {
+                        Console.Write(resident + " ");
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine("----");
+                Car[] cars = new Car[]//3
+                    {
+                     new Car("Ford", "Mustang"),
+                     new Car("Chevrolet", "Camaro"),
+                     new Car("Dodge", "Charger")
+                    };
+
+                Garage garage = new Garage(cars);
+
+                foreach (Car car in garage)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+        }
+    }
+}
